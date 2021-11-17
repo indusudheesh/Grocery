91254528fe4b86bc31877b3c4a60a71bb5bf036c
indusudheesh <93575341+indusudheesh@users.noreply.github.com>
2021-11-17 23:35:35 +0530

    new

---
+++ inv.js
@@ -0,0 +1,49 @@
+var xhttp= new XMLHttpRequest();
+var xhttp=new XMLHttpRequest();
+xhttp.onreadystatechange=function()
+{
+    if(this.readyState==4 && this.status==200)
+    {
+    var response= JSON.parse(this.responseText);
+    var item=response.items;
+    
+var heading=[];
+var tble=document.createElement("table");
+var tr=tble.insertRow(-1);
+        
+    for (var key in item[0])
+     {
+        heading.push(key);
+    }
+    for(let i=0;i<heading.length;i++)
+    {
+        let th = document.createElement("th");
+        th.innerHTML = heading[i];
+        tr.appendChild(th);
+    }
+
+    for (var i = 0; i < item.length; i++)
+     {
+
+        tr = tble.insertRow(-1);
+           
+
+        for (var j = 0; j < heading.length; j++)
+        {
+            var Cell = tr.insertCell(-1);
+        
+            Cell.innerHTML = item[i][heading[j]];
+            
+            
+        }
+    }
+       
+    var inv = document.getElementById("inv");
+    inv.innerHTML = "";
+    inv.appendChild(tble);
+    
+       
+}    
+}
+xhttp.open("GET","inv.json",true);
+xhttp.send();
